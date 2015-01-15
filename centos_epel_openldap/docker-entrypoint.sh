76aed21c7eecc36c01232e92f6c914f7dcf91428
Francisco Miguel Biete Bañon <fbiete@gmail.com>
2015-01-15 16:23:08 +0100

    chore(centos_epel_openldap): Remove initial import

--- centos_epel_openldap/docker-entrypoint.sh
+++ centos_epel_openldap/docker-entrypoint.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e
 
-if [ -z "$SLAPCAT_FILE" ]
-then
-    rm -rf /var/lib/ldap/*
-    slapadd -l $SLAPCAT_FILE
-    rm -f $SLAPCAT_FILE
-fi
-
 chown -R ldap:ldap /etc/openldap/slapd.d
 chown -R ldap:ldap /var/lib/ldap
 
