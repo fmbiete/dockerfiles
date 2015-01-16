aefb582c36848e1c3fa16c7747a37e68dd55dcf7
Francisco Miguel Biete Bañon <fbiete@gmail.com>
2015-01-16 09:30:07 +0100

    chore(centos_epel_openldap): Readd SLAPCAT_FILE

--- centos_epel_openldap/docker-entrypoint.sh
+++ centos_epel_openldap/docker-entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+if [ "$SLAPCAT_FILE" ]; then
+    rm -rf /var/lib/ldap/*
+    slapadd -l $SLAPCAT_FILE -q
+fi
 chown -R ldap:ldap /etc/openldap/slapd.d
 chown -R ldap:ldap /var/lib/ldap
 
