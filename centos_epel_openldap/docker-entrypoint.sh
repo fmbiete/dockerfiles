1606e71dc9523982c47dc3289f6bfd68ac65f056
Francisco Miguel Biete Bañon <fbiete@gmail.com>
2015-01-22 16:54:31 +0100

    fix(centos_epel_openlda): Only import data once
    Importing data in each run will make you lose every change done
    in the openldap database

--- centos_epel_openldap/docker-entrypoint.sh
+++ centos_epel_openldap/docker-entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -e
 
-if [ "$SLAPCAT_FILE" ]; then
-    rm -rf /var/lib/ldap/*
-    slapadd -l $SLAPCAT_FILE -q
+if [ "$SLAPCAT_FILE" ]
+then
+    if [ ! -f /.openldap_loaded ]
+    then
+        rm -rf /var/lib/ldap/*
+        slapadd -l $SLAPCAT_FILE -q
+        touch /.openldap_loaded
+        chown -R ldap:ldap /etc/openldap/slapd.d
+        chown -R ldap:ldap /var/lib/ldap
+    fi
 fi
-chown -R ldap:ldap /etc/openldap/slapd.d
-chown -R ldap:ldap /var/lib/ldap
 
 exec "$@"
